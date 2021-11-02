9c57dba1f03bee03bbdc2e671c27fdc067c3fce8
l3onardodev.github.io <leonardo.contato03@gmail.com>
2021-11-02 19:14:06 -0200

    feat: Destination page done.

--- src/components/DestinationItem.vue
+++ src/components/DestinationItem.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="item">
         <div class="item__img">
-            <img src="../assets/destination/image-europa.png" v-once>
+            <img :src="require(`../assets/destination/image-${title}.png`)">
         </div>
         
         <div class="item-text">
             <nav>
-                <a href="">moon</a>
-                <a href="">mars</a>
-                <a href="">europa</a>
-                <a href="">titan</a>
+                <button :class="{active: title === 'moon'}" @click="changeDestinationItem(1)">moon</button>
+                <button :class="{active: title === 'mars'}" @click="changeDestinationItem(2)">mars</button>
+                <button :class="{active: title === 'europa'}" @click="changeDestinationItem(3)">europa</button>
+                <button :class="{active: title === 'titan'}" @click="changeDestinationItem(4)">titan</button>
             </nav>
 
             <h2>{{ title }}</h2>
@@ -62,13 +62,20 @@ export default {
                 return `${years} YEARS`;
             }
             return `${travelTime} MONTHS`
+        },
+        changeDestinationItem(number) {
+            this.$emit('change-destination-item', number)
         }
     }
 }
 </script>
 
-<style lang="sass">
+<style lang="sass" scoped>
     @import '../sass/variables.scss'
+
+    .active
+        color: #fff !important
+        border-bottom: 3px solid $color_white !important
 
     .item
         display: flex
@@ -88,6 +95,25 @@ export default {
             text-transform: uppercase
             font-family: $text-titles
             margin-bottom: .10em
+
+        nav
+            margin-bottom: 2em
+
+            button
+                border: none
+                background: none
+                font-size: 1rem
+                line-height: 19.2px
+                letter-spacing: 2.7px
+                text-transform: uppercase
+                color: $color_pale_blue
+                margin-right: 2em
+                padding: .8em 0
+                cursor: pointer
+                transition: all .5s
+
+                &:hover
+                    color: $color_white
 
     .item-text__description
         font-size: 1.25rem
